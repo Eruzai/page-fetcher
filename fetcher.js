7998373b8d51ea0169c6a4431a09175da5185acc
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-23 16:45:18 -0800

    app sends message to console detailing the size of the download and the file location

--- fetcher.js
+++ fetcher.js
@@ -12,15 +12,11 @@ const fetcher = function (URL, localFilePath) {
     }
     fs.writeFile(localFilePath, body, (err) => {
       if (err) {
-        console.log("there was an error", err);
+        console.log("there was an error: ", err);
       }
+      console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`)
     });
   });
-  /*download the resource at the URL to the local file path on my machine
-  print out a message when done - "downloaded and saved x bytes to <file path>" 
-  example input and output below:
-  > node fetcher.js http://www.example.edu/ ./index.html
-  Downloaded and saved 3261 bytes to ./index.html*/
 };
 
 fetcher(pageToFetch, saveLocation);
