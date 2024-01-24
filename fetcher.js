014fbabcc91b49194b154fbd87039c2700e3eb8d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-23 16:39:07 -0800

    will store web page in directory location if no error

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,6 @@
 const request = require('request');
+const fs = require('fs');
+
 const pageToFetch = process.argv[2];
 const saveLocation = process.argv[3];
 
@@ -8,7 +10,11 @@ const fetcher = function (URL, localFilePath) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     }
-    console.log('body:', body); // Print the HTML for the page.
+    fs.writeFile(localFilePath, body, (err) => {
+      if (err) {
+        console.log("there was an error", err);
+      }
+    });
   });
   /*download the resource at the URL to the local file path on my machine
   print out a message when done - "downloaded and saved x bytes to <file path>" 
