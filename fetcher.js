0d468bcd75e5a408b22f8bcfec89f49883c980cf
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-24 10:54:08 -0800

    comments, fixed bug with readline causing hangup, app works as intended

--- fetcher.js
+++ fetcher.js
@@ -3,35 +3,37 @@ const fs = require('fs');
 const constants = require('fs');
 const readline = require('readline');
 
-const pageToFetch = process.argv[2];
-const saveLocation = process.argv[3];
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const pageToFetch = process.argv[2]; // accepts command line argument for URL page
+const saveLocation = process.argv[3]; // accepts command line argument for save location
 
-const writeFile = function (filePath, data) {
+const writeFile = function(filePath, data) { // function to write the URL page to a location on the local drive
   fs.writeFile(filePath, data, (err) => {
     if (err) {
-      console.log("there was an error: ", err);
+      console.log("❌ Invalid filepath"); // if the file path isn't valid prints this message to the console
+    } else {
+      console.log(`✅ Downloaded and saved ${data.length} bytes to ${filePath}`); // prints the size and location of the downloaded page
     }
-    console.log(`Downloaded and saved ${data.length} bytes to ${filePath}`)
   });
 };
 
-const fetcher = function (URL, localFilePath) {
+const fetcher = function(URL, localFilePath) {
   request(URL, (error, response, body) => {
     if (error) {
-      console.log("Invalid URL", response); // If the URL doesn't work, prints a message to console.
+      console.log("❌ Invalid URL"); // If the URL doesn't work, prints a message to console.
     } else {
       fs.access(localFilePath, constants.F_OK, (err) => {
         if (err) {
-          writeFile(localFilePath, body);
+          writeFile(localFilePath, body); // if the path doesn't exist (the file doesn't exist) creates and writes to the file
         } else {
+          const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          });
           rl.question("🛑 The file exists at this location! Hit enter to cancel or enter 'Y' to overwrite. ", (answer) => {
-            if (answer.toUpperCase() === 'Y') {
+            if (answer.toUpperCase() === 'Y') { // if the file exists a prompt is generated to ask the user for direction
               writeFile(localFilePath, body);
             }
+            rl.close();
           });
         }
       });
